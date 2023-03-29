cd10b9ce902c0c25e6a1b4c61078b25f103b275e
Daniel Cikora <d.cikora98@gmail.com>
2023-03-29 19:07:01 +0200

    About us Section DONE + Responsive.

--- src/components/pages/AboutUs.tsx
+++ src/components/pages/AboutUs.tsx
@@ -15,24 +15,22 @@ function AboutUs() {
     <nav className='nav-about'>
       <div className='wrapper'>
         <div className='nav-about__content'>
-          <h2 className='title nav-about__title nav-about__h2'>About us</h2>
+          <h2 className='title nav-about__title nav-about__h2'>About us.</h2>
           <div className='nav-about__ourcompany'>
-            <div className='about-ourcompany__text'>
-              <p className='paragraph nav-about__p'>
-                Welcome to Trafalgar, where we are committed to improving the
-                health and well-being of our patients through the delivery of
-                high-quality healthcare services. At our core, we believe that
-                healthcare should be patient-centered and accessible to
-                everyone. We understand that navigating the complexities of the
-                healthcare system can be overwhelming, which is why we strive to
-                provide a welcoming and supportive environment for our patients.
-              </p>
-            </div>
             <img
               className='nav-about__img'
               src={DoctorImage}
               alt='doctor-image'
             />
+            <p className='paragraph nav-about__p'>
+              Welcome to Trafalgar, where we are committed to improving the
+              health and well-being of our patients through the delivery of
+              high-quality healthcare services. At our core, we believe that
+              healthcare should be patient-centered and accessible to everyone.
+              We understand that navigating the complexities of the healthcare
+              system can be overwhelming, which is why we strive to provide a
+              welcoming and supportive environment for our patients.
+            </p>
           </div>
           <h3 className='title nav-about__title nav-about__h3'>We care.</h3>
           <div className='nav-about__wecare'>
@@ -49,10 +47,11 @@ function AboutUs() {
               services, we are here to help.
             </p>
           </div>
-          <h4 className='title nav-about__title nav-about__h4'>
-            Our facilities.
-          </h4>
+
           <div className='nav-about__facilies'>
+            <h4 className='title nav-about__title nav-about__h4'>
+              Our facilities.
+            </h4>
             <p className='paragraph nav-about__p nav-about__p--facilities'>
               Our state-of-the-art facilities are equipped with the latest
               medical technologies, allowing us to deliver advanced diagnostic
@@ -122,7 +121,26 @@ function AboutUs() {
             </Swiper>
           </div>
           <div className='nav-about__partnership'>
-            <h3>Doctors and Patients build a lasting bond</h3>
+            <h5 className='title nav-about__title nav-about__h5'>
+              Build a lasting bond.
+            </h5>
+            <p className='paragraph nav-about__p nav-about__p--building'>
+              At Trafalgar, we believe that healthcare is a partnership between
+              patients and providers. We value open and honest communication,
+              and we encourage our patients to actively participate in their
+              healthcare decisions. Our goal is to empower our patients to take
+              control of their health and to live their best lives.
+            </p>
+            <img
+              className='nav-bonding__img'
+              src={DocAndPatient}
+              alt='doctor-and-patient__image'
+            />
+            <p className='paragraph nav-about__p nav-about__p--mini'>
+              Thank you for considering Trafalgar for your healthcare needs. We
+              look forward to serving you and your family with the highest level
+              of care and professionalism.
+            </p>
           </div>
         </div>
       </div>
