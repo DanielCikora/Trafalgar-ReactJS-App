86e0a54e6c2daef9435d41dfdb253ffddac5a9bf
Daniel Cikora <d.cikora98@gmail.com>
2023-09-24 09:41:35 +0200

    npm updated/24.8.23

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -10,12 +10,9 @@ export default function Article() {
   }
 
   const cardText: cardTextProps = {
-    text1:
-      'In this case, the role of the health laboratory is very important to do a disease detection...that can lead to early intervention and better treatment outcomes.',
-    text2:
-      'Herbal medicine is very widely used at this time because of its very good for your health...and its natural properties often complement traditional medical approaches effectively.',
-    text3:
-      'A healthy lifestyle should start from now and also for your skin health. There are some...simple yet impactful skincare routines that can help maintain a radiant complexion over time.',
+    text1: `In this case, the role of the health laboratory is very important to do a disease detection...that can lead to early intervention and better treatment outcomes.`,
+    text2: `Herbal medicine is very widely used at this time because of its very good for your health...and its natural properties often complement traditional medical approaches effectively.`,
+    text3: `A healthy lifestyle should start from now and also for your skin health. There are some...simple yet impactful skincare routines that can help maintain a radiant complexion over time.`,
   };
 
   return (
