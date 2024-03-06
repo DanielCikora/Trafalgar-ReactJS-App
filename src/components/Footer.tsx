ba3c0e9a2033046decf6f4d92293d216bf5d3163
Daniel Cikora <d.cikora98@gmail.com>
2024-03-06 09:33:04 +0100

    Footer ~ Done, Nav ~ Floating animation added.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,109 @@
-import FooterBox from "./FooterBox/FooterBox";
-export default function Footer() {
+import LogoFooter from "../assets/images/logo-footer.png";
+import Dots from "../assets/images/dots__image.png";
+import { Link } from "react-router-dom";
+export default function FooterBox() {
+  const date = new Date();
+  const fullYear = date.getFullYear();
   return (
     <footer className='footer'>
+      <img className='footer-dots__image' src={Dots} alt='footer-dots' />
       <div className='wrapper'>
-        <div className='footer-logo'></div>
         <div className='footer-links'>
-          <FooterBox />
+          <div className='footer-links__logo'>
+            <Link className='footer-logo' to='/'>
+              <img className='footer-logo__image' src={LogoFooter} alt='logo' />
+            </Link>
+            <p className='footer-logo__p'>
+              Trafalgar provides progressive, and affordable healthcare,
+              accessible on mobile and online for everyone
+            </p>
+            <p className='footer-logo__p footer-logo__p--date'>
+              ©Trafalgar PTY LTD {fullYear}. All rights reserved
+            </p>
+          </div>
+          <div className='footer-links__boxes'>
+            <div className='footer-box'>
+              <ul className='footer-ul'>
+                <li className='footer-ul__li'>
+                  <h2 className='footer-ul__h2'>Company</h2>
+                </li>
+                <li className='footer-ul__li'>
+                  <Link className='link footer-li__a' to='/AboutUs'>
+                    About
+                  </Link>
+                </li>
+                <li className='footer-ul__li'>
+                  <Link className='link footer-li__a' to='/Testimonials'>
+                    Testimonials
+                  </Link>
+                </li>
+                <li className='footer-ul__li'>
+                  <Link className='link footer-li__a' to='/FindDoctor'>
+                    Find a doctor
+                  </Link>
+                </li>
+                <li className='footer-ul__li'>
+                  <Link className='link footer-li__a' to='/Apps'>
+                    Apps
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className='footer-box'>
+              <ul className='footer-ul'>
+                <li className='footer-ul__li'>
+                  <h2 className='footer-ul__h2'>Region</h2>
+                </li>
+                <li className='footer-ul__li'>
+                  <a className='link footer-li__a' href='/'>
+                    Indonesia
+                  </a>
+                </li>
+                <li className='footer-ul__li'>
+                  <a className='link footer-li__a' href='/'>
+                    Singapore
+                  </a>
+                </li>
+                <li className='footer-ul__li'>
+                  <a className='link footer-li__a' href='/'>
+                    Hong Kong
+                  </a>
+                </li>
+                <li className='footer-ul__li'>
+                  <a className='link footer-li__a' href='/'>
+                    Canada
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className='footer-box'>
+              <ul className='footer-ul'>
+                <li className='footer-ul__li'>
+                  <h2 className='footer-ul__h2'>Help</h2>
+                </li>
+                <li className='footer-ul__li'>
+                  <a className='link footer-li__a' href='/'>
+                    Help center
+                  </a>
+                </li>
+                <li className='footer-ul__li'>
+                  <a className='link footer-li__a' href='/'>
+                    Contact support
+                  </a>
+                </li>
+                <li className='footer-ul__li'>
+                  <a className='link footer-li__a' href='/'>
+                    Instructions
+                  </a>
+                </li>
+                <li className='footer-ul__li'>
+                  <a className='link footer-li__a' href='/'>
+                    How it works
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </footer>
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,87 +1,61 @@
-import { useEffect, useState } from 'react';
-import { Twirl as Hamburger } from 'hamburger-react';
-import Logo from '../assets/images/trafalgar-logo__image.png';
-import { Link } from 'react-router-dom';
-function Navigation() {
+import { useEffect, useState } from "react";
+import { Twirl as Hamburger } from "hamburger-react";
+import Logo from "../assets/images/trafalgar-logo__image.png";
+import { Link } from "react-router-dom";
+export default function Navigation() {
   const [isOpen, setOpen] = useState(false);
   const [isFixed, setIsFixed] = useState(false);
 
+  const navItems = [
+    { key: 1, navLink: "Home", href: "/" },
+    { key: 2, navLink: "Find a doctor", href: "/FindDoctor" },
+    { key: 3, navLink: "Apps", href: "/Apps" },
+    { key: 4, navLink: "Testimonials", href: "/Testimonials" },
+    { key: 5, navLink: "About us", href: "/AboutUs" },
+  ];
+
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 0) {
+      if (window.scrollY > 100) {
         setIsFixed(true);
       } else {
         setIsFixed(false);
       }
     };
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
   useEffect(() => {
     if (isOpen) {
-      document.body.classList.add('open');
+      document.body.classList.add("open");
     } else {
-      document.body.classList.remove('open');
+      document.body.classList.remove("open");
     }
   });
 
   return (
     <header>
-      <nav className={`nav ${isFixed ? 'nav--fixed' : ''}`}>
-        <div className="wrapper">
-          <div className="nav__content">
-            <Link to="/">
-              <img src={Logo} alt="trafalgar-logo" loading="lazy" />
+      <nav className={`nav${isFixed ? " nav--fixed" : ""}`}>
+        <div className='wrapper'>
+          <div className='nav__content'>
+            <Link to='/'>
+              <img src={Logo} alt='trafalgar-logo' loading='lazy' />
             </Link>
-            <ul className={isOpen ? 'nav__ul nav__ul--open' : 'nav__ul'}>
-              <li className="nav__li">
-                <Link
-                  to="/"
-                  className="nav__li-a"
-                  onClick={() => setOpen(false)}
-                >
-                  Home
-                </Link>
-              </li>
-              <li className="nav__li">
-                <Link
-                  to="/FindDoctor"
-                  className="nav__li-a"
-                  onClick={() => setOpen(false)}
-                >
-                  Find a doctor
-                </Link>
-              </li>
-              <li className="nav__li">
-                <Link
-                  to="/Apps"
-                  className="nav__li-a"
-                  onClick={() => setOpen(false)}
-                >
-                  Apps
-                </Link>
-              </li>
-              <li className="nav__li">
-                <Link
-                  to="/Testimonials"
-                  className="nav__li-a"
-                  onClick={() => setOpen(false)}
-                >
-                  Testimonials
-                </Link>
-              </li>
-              <li className="nav__li">
-                <Link
-                  to="/AboutUs"
-                  className="nav__li-a"
-                  onClick={() => setOpen(false)}
-                >
-                  About Us
-                </Link>
-              </li>
+            <ul className={isOpen ? "nav__ul nav__ul--open" : "nav__ul"}>
+              {navItems.map((navItem) => (
+                <li className='nav__li' key={navItem.key}>
+                  <Link
+                    className='link nav__li-a'
+                    onClick={() => setOpen(false)}
+                    to={navItem.href}
+                  >
+                    {navItem.navLink}
+                  </Link>
+                </li>
+              ))}
             </ul>
             <Hamburger toggled={isOpen} toggle={setOpen} />
           </div>
@@ -90,4 +64,3 @@ function Navigation() {
     </header>
   );
 }
-export default Navigation;
